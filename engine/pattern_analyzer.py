d610e8e9ab38cab5903a91a554aa281f32811df1
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-10 15:34:20 -0600

    Update pattern_analyzer.py

--- engine/pattern_analyzer.py
+++ engine/pattern_analyzer.py
@@ -1,1 +1,14 @@
 
+from engine.frequency_engine import calculate_frequency
+
+def analyze_patterns(df):
+
+    frequency = calculate_frequency(df)
+
+    sorted_freq = sorted(
+        frequency.items(),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return sorted_freq
