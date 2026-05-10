3d17761a0403ef79e3018f23d04d2ab3e3f2af90
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-10 15:31:56 -0600

    Update style.py

--- app/style.py
+++ app/style.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from config.settings import *
 
+def load_styles():
+
+    st.markdown(f'''
+    <style>
+
+    .stApp {{
+        background-color: {BACKGROUND_COLOR};
+        color: {TEXT_COLOR};
+    }}
+
+    h1,h2,h3 {{
+        color: {PRIMARY_COLOR};
+    }}
+
+    </style>
+    ''', unsafe_allow_html=True)
