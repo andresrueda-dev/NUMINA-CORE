676ac12e322c5a8cf1eaec2c6ca7ae316d2fcef3
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-10 15:41:26 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,4 @@
-import streamlit as st
-
 from app.styles import load_styles
 from app.visualizer import show_header
 from app.upload import upload_dataset
 from app.dashboard import show_dashboard
-
-st.set_page_config(
-    page_title="NUMINA CORE",
-    layout="wide"
-)
-
-load_styles()
-
-show_header()
-
-st.markdown("---")
-
-df = upload_dataset()
-
-if df is not None:
-
-    show_dashboard(df)
-
-else:
-
-    st.info(
-        "Upload a dataset to begin analysis"
-    )
