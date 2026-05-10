aad3821104ce94489a2ad68f4251ee28e594dedb
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-10 15:18:26 -0600

    Create upload.py

---
+++ app/upload.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+def upload_dataset():
+
+    uploaded = st.file_uploader(
+        "Upload CSV Dataset",
+        type=["csv"]
+    )
+
+    if uploaded:
+        df = pd.read_csv(uploaded)
+        return df
+
+    return None
