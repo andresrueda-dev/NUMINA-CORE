e1234e8d65c3090bb5b890a7e1991012e4f1886b
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-10 15:33:28 -0600

    Update frequency_engine.py

--- engine/frequency_engine.py
+++ engine/frequency_engine.py
@@ -1,1 +1,17 @@
+from collections import Counter
 
+def calculate_frequency(df):
+
+    numbers = []
+
+    for col in df.columns:
+
+        if col.startswith("r"):
+
+            numbers.extend(
+                df[col].dropna().tolist()
+            )
+
+    frequency = Counter(numbers)
+
+    return frequency
